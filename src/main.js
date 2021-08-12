6582d9d2e8b06df509f5c36e1e3d380afa518d55
Erpriliano Abbas <erpriliano@gmail.com>
2021-08-13 06:48:07 +0700

    Prepare the template

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import MathFighterScene from './scenes/MathFighterScene'
 
 const config = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: 480,
+	height: 640,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 }
-		}
+		},
 	},
-	scene: [HelloWorldScene]
+	scene: [MathFighterScene],
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH
+	}
 }
 
 export default new Phaser.Game(config)
---
+++ src/scenes/MathFighterScene.js
@@ -0,0 +1,21 @@
+import Phaser from 'phaser'
+
+export default class MathFighterScene extends Phaser.Scene
+{
+	constructor()
+	{
+		super('math-fighter')
+	}
+
+    init() {
+
+    }
+
+	preload() {
+
+    }
+
+    create() {
+        
+    }
+}
