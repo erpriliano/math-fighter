dbee9a1f61ab90c5cf4d84549947479c59f51d31
Erpriliano Abbas <erpriliano@gmail.com>
2021-08-13 06:53:28 +0700

    Preload all assets

--- src/scenes/MathFighterScene.js
+++ src/scenes/MathFighterScene.js
@@ -4,7 +4,7 @@ export default class MathFighterScene extends Phaser.Scene
 {
 	constructor()
 	{
-		super('math-fighter')
+		super('math-fighter-scene')
 	}
 
     init() {
@@ -12,10 +12,20 @@ export default class MathFighterScene extends Phaser.Scene
     }
 
 	preload() {
+        // Image
+        this.load.image('background', 'images/bg_layer1.png')
+        this.load.image('fight-bg', 'images/fight-bg.png')
+        this.load.image('tile', 'images/tile.png')
+        this.load.image('start-btn', 'images/start_button.png')
 
+        // Spritesheet
+        this.load.spritesheet('player', 'images/warrior1.png', { frameHeight: 80, frameWidth: 80 })
+        this.load.spritesheet('enemy', 'images/warrior2.png', { frameHeight: 80, frameWidth: 80 })
+        this.load.spritesheet('numbers', 'images/numbers.png', { frameHeight: 71.25, frameWidth: 131 })
+        this.load.spritesheet('slash', 'images/slash.png', { frameHeight: 88, frameWidth: 42 })
     }
 
     create() {
-        
+
     }
 }
