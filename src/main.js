45c44c3c5e17abd917168bf9d825e19c0ce48432
Erpriliano Abbas <erpriliano@gmail.com>
2021-08-13 07:14:05 +0700

    Add prettierrc for code formatter

--- src/main.js
+++ src/main.js
@@ -3,20 +3,20 @@ import Phaser from 'phaser'
 import MathFighterScene from './scenes/MathFighterScene'
 
 const config = {
-	type: Phaser.AUTO,
-	width: 480,
-	height: 640,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		},
-	},
-	scene: [MathFighterScene],
-	scale: {
-		mode: Phaser.Scale.FIT,
-		autoCenter: Phaser.Scale.CENTER_BOTH
-	}
+    type: Phaser.AUTO,
+    width: 480,
+    height: 640,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 },
+        },
+    },
+    scene: [MathFighterScene],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
 }
 
 export default new Phaser.Game(config)
--- src/scenes/MathFighterScene.js
+++ src/scenes/MathFighterScene.js
@@ -1,11 +1,9 @@
 import Phaser from 'phaser'
 
-export default class MathFighterScene extends Phaser.Scene
-{
-	constructor()
-	{
-		super('math-fighter-scene')
-	}
+export default class MathFighterScene extends Phaser.Scene {
+    constructor() {
+        super('math-fighter-scene')
+    }
 
     init() {
         this.gameHalfWidth = this.scale.width * 0.5
@@ -21,7 +19,7 @@ export default class MathFighterScene extends Phaser.Scene
         this.slash = undefined
     }
 
-	preload() {
+    preload() {
         // Image
         this.load.image('background', 'images/bg_layer1.png')
         this.load.image('fight-bg', 'images/fight-bg.png')
@@ -29,10 +27,22 @@ export default class MathFighterScene extends Phaser.Scene
         this.load.image('start-btn', 'images/start_button.png')
 
         // Spritesheet
-        this.load.spritesheet('player', 'images/warrior1.png', { frameHeight: 80, frameWidth: 80 })
-        this.load.spritesheet('enemy', 'images/warrior2.png', { frameHeight: 80, frameWidth: 80 })
-        this.load.spritesheet('numbers', 'images/numbers.png', { frameHeight: 71.25, frameWidth: 131 })
-        this.load.spritesheet('slash', 'images/slash.png', { frameHeight: 88, frameWidth: 42 })
+        this.load.spritesheet('player', 'images/warrior1.png', {
+            frameHeight: 80,
+            frameWidth: 80,
+        })
+        this.load.spritesheet('enemy', 'images/warrior2.png', {
+            frameHeight: 80,
+            frameWidth: 80,
+        })
+        this.load.spritesheet('numbers', 'images/numbers.png', {
+            frameHeight: 71.25,
+            frameWidth: 131,
+        })
+        this.load.spritesheet('slash', 'images/slash.png', {
+            frameHeight: 88,
+            frameWidth: 42,
+        })
     }
 
     create() {
@@ -41,19 +51,45 @@ export default class MathFighterScene extends Phaser.Scene
         // Create fight-bg
         const fight_bg = this.add.image(240, 160, 'fight-bg')
         // Create tile
-        const tile = this.physics.add.staticImage(240, fight_bg.height - 40, 'tile')
+        const tile = this.physics.add.staticImage(
+            240,
+            fight_bg.height - 40,
+            'tile'
+        )
 
         // Create player
-        this.player = this.physics.add.sprite(this.gameHalfWidth - 150, this.gameHalfHeight - 200, 'player').setOffset(-50, -8).setBounce(0.2)
+        this.player = this.physics.add
+            .sprite(
+                this.gameHalfWidth - 150,
+                this.gameHalfHeight - 200,
+                'player'
+            )
+            .setOffset(-50, -8)
+            .setBounce(0.2)
         // Collide the player with the tile
         this.physics.add.collider(this.player, tile)
 
         // Create enemy
-        this.enemy = this.physics.add.sprite(this.gameHalfWidth + 150, this.gameHalfHeight - 200, 'enemy').setOffset(50, -8).setBounce(0.2).setFlipX(true)
+        this.enemy = this.physics.add
+            .sprite(
+                this.gameHalfWidth + 150,
+                this.gameHalfHeight - 200,
+                'enemy'
+            )
+            .setOffset(50, -8)
+            .setBounce(0.2)
+            .setFlipX(true)
         // Collide the enemy with the tile
         this.physics.add.collider(this.enemy, tile)
 
         // Create slash
-        this.slash = this.physics.add.sprite(240, 60, 'slash').setActive(false).setVisible(false).setGravityY(-500).setOffset(0, -10).setDepth(1).setCollideWorldBounds(true)
+        this.slash = this.physics.add
+            .sprite(240, 60, 'slash')
+            .setActive(false)
+            .setVisible(false)
+            .setGravityY(-500)
+            .setOffset(0, -10)
+            .setDepth(1)
+            .setCollideWorldBounds(true)
     }
 }
